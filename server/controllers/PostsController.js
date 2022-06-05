fd4bd997dec9b211fabed506971a8b241915622c
tta2yta <tta2yta@gmail.com>
2022-06-06 01:28:38 +0300

    update getPost action in postscontroller

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -1,4 +1,20 @@
-export const getPosts =(req, res)=>{
-    res.send('hello')
+import PostMessage from "../Models/postMessages.js"
+
+export const getPosts =async (req, res)=>{
+    try{
+        const postMessage= await PostMessage.find()
+        return res.status(200).json(postMessage);
+
+    }catch(error){
+        res.status(400).json({message: error.message})
+    }
+}
+
+export const createPost=(req, res)=>{
+    try{
+
+    }catch(error){
+
+    }
 
 }
