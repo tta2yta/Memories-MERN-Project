5744a05fa6a97a64365200dd12cd72505ca22c88
tta2yta <tta2yta@gmail.com>
2022-06-06 01:08:19 +0300

    add first route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from "cors"
 
+import postRoutes from './routes/posts.js'
+
 const app=express();
 
 app.use(bodyParser.json({limit:"30mb", extended:true}));
@@ -12,9 +14,14 @@ app.use(cors());
 const CONNECTION_URL= "mongodb+srv://tta2yta:07162078@cluster0.2hvf3d1.mongodb.net/?retryWrites=true&w=majority"
 const PORT= process.env.PORT || 5000
 
+
+//connect to mongodb
 mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=>app.listen(PORT, ()=>console.log("Server is running on port 5000")))
 .catch((error)=>console.log(error.message))
 
-// mongoose.set('useFindAndModify', false)  //false--ignore warnings
+//routes
+app.use('/posts', postRoutes)
+
+
 
