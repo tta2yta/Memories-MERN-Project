de9a54aface6bcb53e3ac1e8a3b621da21ccff37
tta2yta <tta2yta@gmail.com>
2022-06-05 23:27:17 +0300

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -8,3 +8,13 @@ const app=express();
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 app.use(cors());
+
+const CONNECTION_URL= "mongodb+srv://tta2yta:07162078@cluster0.2hvf3d1.mongodb.net/?retryWrites=true&w=majority"
+const PORT= process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+.then(()=>app.listen(PORT, ()=>console.log("Server is running on port 5000")))
+.catch((error)=>console.log(error.message))
+
+// mongoose.set('useFindAndModify', false)  //false--ignore warnings
+
