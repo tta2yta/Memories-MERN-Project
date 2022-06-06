4b708a94ab6d828476f1b3fd39289aab257dda21
tta2yta <tta2yta@gmail.com>
2022-06-06 14:44:53 +0300

    Adding styles to components

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import useStyles from './styles'
 
 function Form() {
+  const classes= useStyles()
   return (
     <h1>Form</h1>
   )
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Post from './Post/Post'
 
 function Posts() {
+  const classes= useStyles()
   return (
     <>
         <h1>Posts</h1>
