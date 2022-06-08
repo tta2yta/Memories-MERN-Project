13612dfbd6517aa8eb78a2045afcf926cf8817c4
tta2yta <tta2yta@gmail.com>
2022-06-08 16:04:22 +0300

    add form post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import useStyles from './style.js'
-import {useSelector} from 'react-redux'
-
+import React from "react";
+import useStyles from "./style.js";
+import { useSelector } from "react-redux";
 
 function Post() {
-  const posts= useSelector((state)=>state.posts);
-  const classes= useStyles()
-  console.log(posts)
-  return (
-    <div>Post</div>
-  )
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+  console.log(posts);
+  return <div>Post</div>;
 }
 
-export default Post
+export default Post;
