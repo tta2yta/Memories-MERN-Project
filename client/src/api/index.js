98de85adc2ff52c119637cd119ce34ef5e29c4a9
tta2yta <tta2yta@gmail.com>
2022-06-13 13:12:49 +0300

    add api for likepost in client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -18,3 +18,4 @@ export const updatePost = (id, updatedPost) =>
   axios.patch(`${url}/${id}`, updatedPost);
 
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
